24dfe0e409cbfaa6d5b9c38e467504e27b4e97dd
Breydan <me@breydan.dev>
2021-03-30 12:54:47 -0400

    Update Settings.java

--- UHC/src/main/java/xyz/breydan/uhc/settings/Settings.java
+++ UHC/src/main/java/xyz/breydan/uhc/settings/Settings.java
@@ -8,12 +8,6 @@ public class Settings {
 
     private final YamlConfig config;
 
-    @Value("mongo_uri")
-    public static String URI = "mongodb://localhost:27017/tag";
-
-    @Value("mongo_db")
-    public static String DB = "tag";
-
     @Value("border_change_interval_mins")
     public static int BORDER_CHANGE_INTERVAL_MINS = 10;
 
